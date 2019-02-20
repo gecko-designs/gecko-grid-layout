be48fd4921c157b29953fe7d8eecbb47464e48b9
Dwayne Parton <dwayne.parton@geckodesigns.com>
2019-02-20 14:49:30 -0700

    Removed FontAwesome

--- src/grid-layout.js
+++ src/grid-layout.js
@@ -1,11 +1,8 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTh } from '@fortawesome/free-solid-svg-icons';
-
 /**
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { PanelBody, RangeControl} from '@wordpress/components';
+import { PanelBody, RangeControl, G, SVG, Path } from '@wordpress/components';
 import { Fragment } from '@wordpress/element';
 import {
 	InspectorControls,
@@ -26,9 +23,9 @@ const ALLOWED_BLOCKS = ['gecko/grid-layout-item'];
 export const name = 'gecko/grid-layout';
 
 export const settings = {
-	title: __( 'Grid Layout' ),
+	title: __( 'Grid' ),
 
-	icon: <FontAwesomeIcon icon={faTh} />,
+	icon: <SVG viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><Path fill="none" d="M0 0h24v24H0V0z" /><G><Path d="M4,13 L8,13 L8,10 L4,10 L4,13 Z M4,15 L4,18 L8,18 L8,15 L4,15 Z M10,13 L14,13 L14,10 L10,10 L10,13 Z M10,15 L10,18 L14,18 L14,15 L10,15 Z M20,13 L20,10 L16,10 L16,13 L20,13 Z M20,15 L16,15 L16,18 L20,18 L20,15 Z M4,8 L8,8 L8,5 L4,5 L4,8 Z M10,8 L14,8 L14,5 L10,5 L10,8 Z M20,8 L20,5 L16,5 L16,8 L20,8 Z M4,3 L20,3 C21.1045695,3 22,3.95139491 22,5.125 L22,17.875 C22,19.0486051 21.1045695,20 20,20 L4,20 C2.8954305,20 2,19.0486051 2,17.875 L2,5.125 C2,3.95139491 2.8954305,3 4,3 Z" /></G></SVG>,
 
 	category: 'layout',
 
@@ -43,7 +40,7 @@ export const settings = {
 		},
 	},
 
-	description: __( 'Add a block that displays content in multiple columns, then add whatever content blocks you’d like.' ),
+	description: __( 'Use CSS Grid to build layouts and, then add whatever content blocks you’d like.' ),
 
 	supports: {
 		align: [ 'wide', 'full' ],
@@ -52,7 +49,7 @@ export const settings = {
 
 	deprecated: [],
 
-	edit( { attributes, setAttributes, className } ) {
+	edit( { attributes, setAttributes } ) {
 		const { columns, gap } = attributes;
 		const styles = 'grid-template-columns: repeat(' + columns + ', minmax(100px, 1fr));	grid-auto-rows: minmax(100px, auto); grid-gap:' + gap + 'rem;';
 
@@ -61,7 +58,7 @@ export const settings = {
 				<InspectorControls>
 					<PanelBody>
 						<RangeControl
-							label={ __( 'Columns' ) }
+							label={ __( 'Grid Layout' ) }
 							value={ columns }
 							onChange={ ( nextColumns ) => {
 								setAttributes( {
